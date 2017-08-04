ecf637b5e67eb572f4e362b235bc80d2e28b8a66
Adam C. Johnson <adacjohn@gmail.com>
2017-08-04 15:47:31 -0400

    Update PingScript.bash

--- Utilities/PingScript.bash
+++ Utilities/PingScript.bash
@@ -5,7 +5,7 @@ then
 echo "Usage: ./pingscript.sh [network]"
 echo "Example: ./pingscript.sh 192.168.0"
 else
-echo "Please note that if ICMP is disabled, pings will not show hosts."
+echo "Please note that if ICMP is disabled, hosts will not respond."
 for x in {1..254}; do
 ping -c 1 $1.$x | grep "64 bytes" | cut -d" " -f4 | sed 's/.$//' 
 done
